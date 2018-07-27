8f0562db544cb4e05c46b09cd9302b43c5c16eac
Joaquín Herrera <herrerab.joaquin@gmail.com>
2018-07-27 22:47:04 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -22,6 +22,6 @@ end
 
 
 #  @session = session
-#  The controller action /checkout, should take the params from the form and add it to the session hash. 
-#  The key should be item and the value should be the item the user entered to the purchase. Make sure to store the session 
+#  The controller action /checkout, should take the params from the form and add it to the session hash.
+#  The key should be item and the value should be the item the user entered to the purchase. Make sure to store the session
 #  hash in an instance variable that you can access in the views.
